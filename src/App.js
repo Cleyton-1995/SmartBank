47e238ca6820b4612824f9e940dcfc45cce27421
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2024-11-02 09:59:42 -0300

    Tems

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 import Cabecalho from "./Components/Cabecalho";
-import  { ContainerRapper } from "./Components/Container";
+import { ContainerRapper } from "./Components/Container";
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import { temaEscuro, temaClaro } from "./Components/UI/temas";
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={{temaEscuro, temaClaro}}>
       <GlobalStyle />
       <Cabecalho />
       <ContainerRapper />
-    </>
+    </ThemeProvider>
   );
 }
 
